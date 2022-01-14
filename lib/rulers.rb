8791742448d03e111939dba580b26afce7a9ba85
Anna Winkler <3526523+annawinkler@users.noreply.github.com>
2022-01-14 17:56:05 +0400

    Add add for arrays

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,12 @@
-require "rulers/version"
+require 'rulers/array'
+require 'rulers/version'
 
 module Rulers
   class Error < StandardError; end
 
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      `echo debug > debug.txt`
       [200, {'Content-Type' => 'text/html'},
        ["Hello from Ruby on Rulers!"]]
     end
